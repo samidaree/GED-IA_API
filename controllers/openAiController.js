e2889f35b0252fca6dff35fd4e7f67a0dd4c05af
Sami <117381937+samidaree@users.noreply.github.com>
2023-04-25 20:27:39 +0200

    database requests

--- controllers/openAiController.js
+++ controllers/openAiController.js
@@ -1,5 +1,73 @@
 
+const mysql = require('mysql');
 
+const saveDatabase = async (req, res) => {
+    const name = mysql.escape(req.body.name);
+    const resumeStr = req.body.summary;
+    const motscleStr = req.body.keywords;
+
+    console.log("resume " + resumeStr);
+    console.log("keywords " + motscleStr)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: '8889',
+        user: 'root',
+        password: 'root',
+        database: 'database'
+    });
+
+    try {
+        // First, check if a tuple with the given name already exists
+        connection.query(
+            `SELECT id FROM ged WHERE nom = ${name}`,
+            (error, results) => {
+                if (error) {
+                    res.status(500).send("Error connection database");
+                    throw error;
+
+                }
+                // If a tuple with the given name exists, update it
+                if (results.length > 0) {
+                    const id = results[0].id;
+                    connection.query(
+                        `UPDATE ged SET resume = ${mysql.escape(resumeStr)}, motscle = ${mysql.escape(motscleStr)} WHERE id = '${id}'`,
+                        (error, results) => {
+                            if (error) throw error;
+                            console.log(`Updated tuple with id = ${id}`);
+                            connection.end();
+                            res.status(200).send("Tuple updated successfully");
+                        }
+                    );
+                }
+                // Otherwise, create a new tuple with the next available id
+                else {
+                    connection.query(
+                        `SELECT MAX(id) as max_id FROM ged`,
+                        (error, results) => {
+                            if (error) throw error;
+
+                            const nextId = results.length > 0 ? results[0].max_id + 1 : 1;
+
+                            connection.query(
+                                `INSERT INTO ged (id, nom, resume, motscle) VALUES (${nextId}, ${name}, ${mysql.escape(resumeStr)}, ${mysql.escape(motscleStr)})`,
+                                (error, results) => {
+                                    if (error) throw error;
+                                    console.log(`Created new tuple with id = ${nextId}`);
+                                    connection.end();
+                                    res.status(200).send("Tuple created successfully");
+                                }
+                            );
+                        }
+                    );
+                }
+            }
+        );
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Database operation failed");
+    }
+};
 
 const generateSummary = async (req, res) => {
 
@@ -11,27 +79,8 @@ const generateSummary = async (req, res) => {
 
     const openai = new OpenAIApi(configuration)
     const articleData = req.body.articleData;
-    //console.log("recu : " + articleData);
 
-    /*const prompt = "fait un résumé de 5 lignes du texte suivant : ";
-    try {
-        const response = await openai.createCompletion({
-
-            model: "text-davinci-003",
-            prompt: "donne moi un fait psychologique interessant",
-            temperature: 0.5,
-            max_tokens: 500,
-            top_p: 1.0,
-            frequency_penalty: 0.8,
-            presence_penalty: 0.0,
-        });
-        console.log(response.data.choices);
-        console.log(response.data.choices[0].text)
-
-    } catch (error) {
-        console.log(error)
-    } */
-
+    const name = mysql.escape(req.body.name);
     const summary = {};
 
     for (const key in articleData) {
@@ -81,6 +130,7 @@ const generateKeys = async (req, res) => {
     const openai = new OpenAIApi(configuration)
     console.log("Fonction mots clés");
     const articleData = req.body.articleData;
+    const name = mysql.escape(req.body.name);
 
     const keywords = {};
     let prompt = req.body.prompt
@@ -124,10 +174,12 @@ const generateKeys = async (req, res) => {
             }
         }
     }
+
     res.json({ keywords: keywords });
 }
 
 module.exports = {
     generateSummary,
-    generateKeys
+    generateKeys,
+    saveDatabase
 }
--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,13 @@ const express = require("express");
 const { generateSummary } = require("../controllers/openAiController");
 
 const { generateKeys } = require("../controllers/openAiController");
+const { saveDatabase } = require("../controllers/openAiController")
 
 const router = express.Router();
 
 router.post("/text", generateSummary)
 router.post("/key", generateKeys)
+router.post("/db", saveDatabase)
+
 
 module.exports = router; 
